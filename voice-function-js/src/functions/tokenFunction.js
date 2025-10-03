f88b7c9efe9ec75778110ef1dff786c2eca1e6e9
Vladlen Silin <vladlen.silin@microsoft.com>
2025-10-03 12:43:54 +0200

    feature: back node.js

---
+++ voice-function-js/src/functions/tokenFunction.js
@@ -0,0 +1,17 @@
+const { app } = require('@azure/functions');
+const { DefaultAzureCredential } = require("@azure/identity");
+
+app.http('token', {
+    route: 'AzureOpenAI/token',
+    methods: ['GET', 'POST'],
+    authLevel: 'function',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const credential = new DefaultAzureCredential();
+
+        const token = await credential.getToken("https://ai.azure.com/.default");
+
+        return { body: token.token };
+    }
+});
